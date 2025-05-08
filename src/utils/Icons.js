b3582a08d18a88cdf2371e59096408495a7bc281
yashF60 <yash.mali@f60host.in>
2025-05-08 17:38:14 +0530

    Converted logos to Lucide Icons

--- src/utils/Icons.js
+++ src/utils/Icons.js
@@ -1,42 +1,89 @@
+// import {
+//   CiTextAlignLeft,
+//   CiTextAlignCenter,
+//   CiTextAlignRight,
+//   CiTextAlignJustify,
+// } from "react-icons/ci";
+// import { MdFormatListBulleted, MdFormatListNumbered } from "react-icons/md";
+// import { BiFontColor } from "react-icons/bi";
+// import { FaSquare } from "react-icons/fa";
+// import { RxFontBold, RxFontItalic, RxUnderline } from "react-icons/rx";
+// import { HiOutlineStrikethrough } from "react-icons/hi2";
+// import { IoMdLink, IoIosClose } from "react-icons/io";
+// import { BsCardImage } from "react-icons/bs";
+
+// export const Icons = {
+//   // Alignment Icons
+//   AlignLeft: CiTextAlignLeft,
+//   AlignCenter: CiTextAlignCenter,
+//   AlignRight: CiTextAlignRight,
+//   AlignJustify: CiTextAlignJustify,
+
+//   // List Icons
+//   UnorderedList: MdFormatListBulleted,
+//   OrderedList: MdFormatListNumbered,
+
+//   // Color Icons
+//   FontColor: BiFontColor,
+//   HighlightSquare: FaSquare,
+
+//   // Operations Icons
+//   Bold: RxFontBold,
+//   Italic: RxFontItalic,
+//   Underline: RxUnderline,
+//   Strikethrough: HiOutlineStrikethrough,
+
+//   // Meta Icons
+//   Link: IoMdLink,
+//   Image: BsCardImage,
+
+//   // Modal Icon
+//   Close: IoIosClose,
+// };
+
 import {
-  CiTextAlignLeft,
-  CiTextAlignCenter,
-  CiTextAlignRight,
-  CiTextAlignJustify,
-} from "react-icons/ci";
-import { MdFormatListBulleted, MdFormatListNumbered } from "react-icons/md";
-import { BiFontColor } from "react-icons/bi";
-import { FaSquare } from "react-icons/fa";
-import { RxFontBold, RxFontItalic, RxUnderline } from "react-icons/rx";
-import { HiOutlineStrikethrough } from "react-icons/hi2";
-import { IoMdLink, IoIosClose } from "react-icons/io";
-import { BsCardImage } from "react-icons/bs";
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+  AlignJustify,
+  List,
+  ListOrdered,
+  Pen,       // Replaces TextColor
+  Square,
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
+  Link,
+  Image,
+  X
+} from "lucide-react";
 
 export const Icons = {
   // Alignment Icons
-  AlignLeft: CiTextAlignLeft,
-  AlignCenter: CiTextAlignCenter,
-  AlignRight: CiTextAlignRight,
-  AlignJustify: CiTextAlignJustify,
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+  AlignJustify,
 
   // List Icons
-  UnorderedList: MdFormatListBulleted,
-  OrderedList: MdFormatListNumbered,
+  UnorderedList: List,
+  OrderedList: ListOrdered,
 
   // Color Icons
-  FontColor: BiFontColor,
-  HighlightSquare: FaSquare,
+  FontColor: Pen, // Use Droplet as a color-related icon
+  HighlightSquare: Square,
 
   // Operations Icons
-  Bold: RxFontBold,
-  Italic: RxFontItalic,
-  Underline: RxUnderline,
-  Strikethrough: HiOutlineStrikethrough,
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
 
   // Meta Icons
-  Link: IoMdLink,
-  Image: BsCardImage,
+  Link,
+  Image,
 
   // Modal Icon
-  Close: IoIosClose,
+  Close: X,
 };
